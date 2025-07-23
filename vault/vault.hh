9f2e441c75217673f78dd12fb5cf94e77a61bbc5
Cezarrr9 <cezar.bulancea2003@gmail.com>
2025-07-24 02:50:03 +0300

    also the header

--- vault/vault.hh
+++ vault/vault.hh
@@ -1,13 +1,21 @@
 #ifndef INCLUDED_VAULT_
 #define INCLUDED_VAULT_
 
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <sodium.h>
 #include <sqlite3.h>
 #include <stdio.h>
 #include <string>
+#include <vector>
+
 
 class Vault
 {
     sqlite3 *d_table;
+    std::array<std::uint8_t, crypto_aead_xchacha20poly1305_IETF_KEYBYTES> d_key;
+    bool d_keyValid = false;
 
     public:
         Vault(std::string const &filename = "vault.db");
@@ -25,6 +33,13 @@ class Vault
         bool get(std::string const &website, std::string const &userIdentifier);
         bool erase(std::string const &website, std::string const &userIdentifier);
         void list();
+
+    private:
+        void openDatabase(std::string const &filename);
+        void ensureSchema();
+        void deriveSessionKey();       // Argon2-id
+        std::vector<std::uint8_t> loadOrCreateSalt();
+        std::string hiddenPrompt(std::string const &prompt);
 };
 
 #endif
