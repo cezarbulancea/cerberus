62c81ef7fb74f32c978d919e1662036bc7fe12ff
Cezar Bulancea <cezar.bulancea@gmail.com>
2025-08-12 18:49:21 +0300

    switched to sqlite_open_v2

--- dbHandle/dbHandle1.cc
+++ dbHandle/dbHandle1.cc
@@ -1,11 +1,14 @@
 #include "dbHandle.ih"
 
 DbHandle::DbHandle(string const &filename)
-{                                      // Open (or create) the database
-    if (sqlite3_open(filename.c_str(), &d_db) != SQLITE_OK)
+{   
+    size_t flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX;
+    if (sqlite3_open_v2(filename.c_str(), &d_db, flags, nullptr) != SQLITE_OK)
     {
-        string const errorMessage = sqlite3_errmsg(d_db);
-        sqlite3_close(d_db);
+        string const errorMessage = d_db ? 
+            sqlite3_errmsg(d_db) : "sqlite3_open_v2 failed";
+        if (d_db)
+            sqlite3_close(d_db);
         throw runtime_error("Failed to open the database: " + errorMessage);
     }
 }
